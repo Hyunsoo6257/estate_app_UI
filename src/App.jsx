42302c7d832e5952cb25c37cfd22520641b7c241
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2024-06-28 17:27:21 +0900

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
 function App() {
   return (
     <div className="layout">
-      <Navbar></Navbar>
+      <Navbar />
     </div>
   );
 }
