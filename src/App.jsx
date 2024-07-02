6d14e87d1acf029b5d1063043c2398e9232d8813
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2024-07-02 22:57:25 +1000

    adding searchBar,listPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
+import HomePage from "./routes/hompage/hompage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
 function App() {
-  return (
-    <div className="layout">
-      <Navbar />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
