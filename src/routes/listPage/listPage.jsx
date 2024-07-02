9c48e817eb020fb7f95ff459de6db5859a572f30
Molly <162527916+Hyunsoo6257@users.noreply.github.com>
2024-07-03 00:14:58 +1000

    adding filter, card UI

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,5 +1,7 @@
 import { listData } from "../../lib/dummydata";
 import "./listPage.scss";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
 
 function ListPage() {
   const data = listData;
@@ -14,9 +16,7 @@ function ListPage() {
           ))}
         </div>
       </div>
-      <div className="mapContainer">
-        <Map items={data} />
-      </div>
+      <div className="mapContainer">{/* <Map items={data} /> */}</div>
     </div>
   );
 }
